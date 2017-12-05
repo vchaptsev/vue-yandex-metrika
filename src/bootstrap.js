1a1c269c66cdd53d6f4759e38b15a25f32f661eb
Victor Chaptsev <victor@chaptsev.ru>
2017-12-05 17:44:35 +0300

    refactor: config, updateConfig are simplified
    merge function removed, defaultConfig removed

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -3,9 +3,7 @@ import config from './config'
 export default function bootstrap (Vue) {
     if (typeof document === 'undefined') {return}
 
-    const { id, ignoreRoutes, skipSamePath, onlyProd, env } = config
-
-    if (!id) {throw new Error('[vue-yandex-metrika] Please enter a Yandex Metrika tracking ID')}
+    if (!config.id) {throw new Error('[vue-yandex-metrika] Please enter a Yandex Metrika tracking ID')}
 
     // Load script (creates DOM-element)
     return new Promise((resolve, reject) => {
@@ -28,20 +26,20 @@ export default function bootstrap (Vue) {
 
     .then(() => {
         // do not create Metrika if onlyProd and env !== "production"
-        if (onlyProd && env !== "production") {
-            return console.log('[vue-yandex-metrika] Tracking is disabled, because onlyProd option is true and env option is', env)
+        if (config.onlyProd && config.env !== "production") {
+            return console.log('[vue-yandex-metrika] Tracking is disabled, because onlyProd option is true and env option is', config.env)
         }
 
         // Create Metrika
-        Vue.prototype.$metrika = Vue.$metrika = new Ya.Metrika({id: id})
+        Vue.prototype.$metrika = Vue.$metrika = new Ya.Metrika({id: config.id})
 
         // Run page autotracking
         config.router.afterEach(function (to, from) {
             // check if route is in ignoreRoutes
-            if (ignoreRoutes.includes(to.name)) {return}
+            if (config.ignoreRoutes.includes(to.name)) {return}
 
             // do not track page visit if previous and next routes URLs match
-            if (skipSamePath && to.path == from.path) {return}
+            if (config.skipSamePath && to.path == from.path) {return}
 
             // track page visit
             Vue.$metrika.hit(to.path, {referer: from.path})
--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const defaultConfig = {
+let config = {
     id: null,
     router: null,
 
@@ -8,22 +8,12 @@ const defaultConfig = {
     skipSamePath: false
 }
 
-let config = { ...defaultConfig }
-
 export function updateConfig (params) {
     // Set env param to passed value or process.env or "production"
     params['env'] = params['env'] || process.env.NODE_ENV || "production"
 
-    function merge (obj, src) {
-        Object.keys(src).forEach(function (key) {
-            if (obj[key] && typeof obj[key] === 'object') {merge(obj[key], src[key]); return}
-            obj[key] = src[key]
-        })
-
-      return obj
-    }
-
-    merge(config, params)
+    // Merge default config and plugin options
+    Object.keys(params).forEach(function (key) {config[key] = params[key]})
 }
 
 export default config
