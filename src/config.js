095147dc89cd2563e70635b4a66b154532143292
dramoturg <dramoturg@gmail.com>
2018-02-05 01:34:27 +0400

    Update config.js
    add parameters for initializing the counter in config

--- src/config.js
+++ src/config.js
@@ -1,7 +1,20 @@
 let config = {
     id: null,
+    options: {
+        accurateTrackBounce: false,
+        clickmap: false,
+        defer: false,
+        ecommerce: false,
+        params: [],
+        userParams: {},
+        trackHash: false,
+        trackLinks: false,
+        type: 0,
+        webvisor: false,
+        triggerEvent: false
+        
+    },
     router: null,
-
     env: 'development',
     debug: false,
     ignoreRoutes: [],
